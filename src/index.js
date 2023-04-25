378eef0364f30e383ad27c1eacab2ea07fae9b8f
turtleDev <saravjeetamansingh@gmail.com>
2023-04-25 14:32:26 +0530

    basic refactor

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {Router} from 'itty-router';
+import { Router } from 'itty-router';
 
 const getGameId = path => {
 	const match = /\/game\/live\/(.*)/.exec(path)
@@ -8,18 +8,14 @@ const getGameId = path => {
 	return match[1];
 };
 
-const getChessDotComPGN = async (linkUrl) => {
-	const gameId = getGameId(linkUrl.pathname);
-	if (!gameId) {
-		return new Response('unable to parse gameId from link', {status: 400})
-	}
-	const gameData = await fetch(`https://www.chess.com/callback/live/game/${gameId}`)
-	if (!gameData.ok) {
-		return new Response(`unable to fetch game data for game id: ${gameId}`)
-	}
-	const game = await gameData.json();
-	const players = Object.values(game.players).map(player => player.username);
-	const date = game.game.pgnHeaders.Date.split('.').slice(0, 2).join('/')
+const pgnFromGameData = async (gameData) => {
+	let { players, game } = await gameData.json();
+	players = Object.values(players).map(player => player.username);
+	const date = game.pgnHeaders.Date
+		.split('.')
+		.slice(0, 2)
+		.join('/');
+
 	for (let i = 0; i < players.length; i++) {
 		const player = players[i];
 		const monthlyGamesLink = `https://api.chess.com/pub/player/${player}/games/${date}`;
@@ -27,17 +23,30 @@ const getChessDotComPGN = async (linkUrl) => {
 		if (playerMonthlyGames.ok) {
 			const { games } = await playerMonthlyGames.json();
 			for (let g = 0; g < games.length; g++) {
-				if (games[g].url.endsWith(gameId)) {
-					return new Response(games[g].pgn, {
-						headers: {
-							"content-type": "application/x-chess-pgn"
-						}
-					});
+				const candidateGame = games[g];
+				if (candidateGame.url.endsWith(game.id)) {
+					return candidateGame.pgn;
 				}
 			}
 		}
 	}
-	return new Response(`unable to locate pgn for game id: ${gameId}`, { status: 400 });
+	return null;
+}
+
+const getChessDotComPGN = async (linkUrl) => {
+	const gameId = getGameId(linkUrl.pathname);
+	if (!gameId) {
+		return new Response('unable to parse gameId from link', { status: 400 })
+	}
+	const gameData = await fetch(`https://www.chess.com/callback/live/game/${gameId}`)
+	if (!gameData.ok) {
+		return new Response(`unable to fetch game data for game id: ${gameId}`)
+	}
+	const pgn = await pgnFromGameData(gameData)
+	if (!pgn) {
+		return new Response(`unable to locate pgn for game id: ${gameId}`, { status: 400 });
+	}
+	return new Response(pgn, { headers: { 'content-type': 'application/x-chess-pgn' } });
 };
 
 const router = Router();
